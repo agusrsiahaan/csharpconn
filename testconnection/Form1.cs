0945dcb82ac86ee416129c4fed60d7de79c30704
agusrsiahaan <agusronaldo2@gmail.com>
2022-03-21 18:47:49 +0700

    init

---
+++ testconnection/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace testconnection
+{
+    public partial class Form1 : Form
+    {
+
+        private SqlCommand cmd;
+        private DataSet ds;
+        private SqlDataAdapter da;
+
+        Koneksi Konn = new Koneksi();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = Konn.GetConn();
+            try
+            {
+                conn.Open();
+                cmd = new SqlCommand("select * from tbl_barang", conn);
+                ds = new DataSet();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds, "tbl_barang");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "tbl_barang";
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            catch(Exception G)
+            {
+                MessageBox.Show(G.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ testconnection/Koneksi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace testconnection
+{
+    class Koneksi
+    {
+        public SqlConnection GetConn()
+        {
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = @"Data Source = AGUS-PC\SQLEXPRESS; initial catalog=db_contoh; integrated security=true";
+            return Conn;
+        }
+    }
+}
